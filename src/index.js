f57c6d250266a870485128276d827ef7e4a6c731
shayan1234554321 <kshayan091@gmail.com>
2023-03-09 00:53:44 +0500

    Added API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
+import Scores from './modules/object.js';
+import { upload, get } from './modules/API.js';
 
 /* eslint-disable no-undef */
 const toast = Swal.mixin({
@@ -16,14 +18,43 @@ const toast = Swal.mixin({
 const form = document.getElementById('form');
 const name = document.getElementById('name');
 const score = document.getElementById('score');
+const scores = document.getElementById('scores');
+const data = new Scores();
+// -------------------- Functions -----------------
+
+const UpdateScoresHTML = () => {
+  scores.innerHTML = '';
+  data.array.forEach((element) => {
+    const li = document.createElement('li');
+    const h4 = document.createElement('h4');
+    h4.innerHTML = ` ${element.user} - ${element.score} `;
+    li.appendChild(h4);
+    scores.appendChild(li);
+  });
+};
+
+const formFunction = () => {
+  data.add({ user: name.value, score: score.value });
+  upload({ user: name.value, score: score.value }).then(() => {
+    UpdateScoresHTML();
+  });
+  name.value = '';
+  score.value = '';
+  toast.fire({
+    title: 'Score added',
+  });
+};
 
 // -------------------- Event listeners -----------------
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  name.value = '';
-  score.value = '';
-  toast.fire({
-    title: 'Score added',
+  formFunction();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  get().then((res) => {
+    data.array = res.result;
+    UpdateScoresHTML();
   });
 });
---
+++ src/modules/object.js
@@ -0,0 +1,11 @@
+class Scores {
+  constructor() {
+    this.array = [];
+  }
+
+  add(data) {
+    this.array.push(data);
+  }
+}
+
+export default Scores;
